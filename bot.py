fe4319be0db49c76f48ef395018dd5520579a54d
CubicPill <CubicPill@users.noreply.github.com>
2020-05-15 02:20:34 +0800

    refactor, upgrade api, fix bug for json storage, add mention reply

--- bot.py
+++ bot.py
@@ -7,17 +7,19 @@ from threading import Lock
 import jieba.posseg
 from telegram import Chat
 from telegram.ext import Updater, Filters, CommandHandler, MessageHandler
+from telegram.messageentity import MessageEntity
 
 logging.basicConfig(level=logging.INFO)
 DEFAULT_P = 0.1
 suffix_words = ['个屁', '个头', '个几把', '个鬼', '个卵']
-my_uid = 0
+bot_info = None
 group_settings = dict()
 settings_lock = Lock()
 character_replacement_data = dict()
+STICKER_FILE_ID = 'CAACAgUAAxkBAAMdXr2GlOY6nXjYR__c-f1yO9UcA48AAqYAA4XhawuRHo4uZmGeyBkE'
 
 
-def test(bot, update):
+def test(update, context):
     update.message.reply_text(random.choice(['测个屁', '测个头']))
 
 
@@ -29,15 +31,27 @@ def generate_repeat_text(text):
     return output
 
 
-def set(bot, update, args):
-    if len(args) != 1:
+def generate_insult_text(text):
+    suffix_word = random.choice(suffix_words)
+    keywords = list()
+    input_words = jieba.posseg.cut(text)
+    for w in input_words:
+        if w.flag.startswith('v') or w.flag.startswith('a') or w.flag == 'i':
+            keywords.append(w.word)
+    if keywords:
+        return random.choice(keywords) + suffix_word
+    return None
+
+
+def set(update, context):
+    if len(context.args) != 1:
         update.message.reply_text('Usage: /setp <Reply Possibility> ([0.00~1.00])')
         return
     if update.message.chat.type not in [Chat.GROUP, Chat.SUPERGROUP]:
         update.message.reply_text('Only available in group chats')
         return
     try:
-        new_p = round(float('0' + args[0]), 6)
+        new_p = round(float('0' + context.args[0]), 6)
         if new_p > 1:
             new_p = 1
         if new_p < 0:
@@ -50,60 +64,71 @@ def set(bot, update, args):
     update.message.reply_text('Success! P = {0:.6f}'.format(new_p))
 
 
-def get(bot, update):
+def get(update, context):
     if update.message.chat.type not in [Chat.GROUP, Chat.SUPERGROUP]:
         update.message.reply_text('Only available in group chats')
         return
     if update.message.chat.id not in group_settings:
         group_settings[update.message.chat.id] = DEFAULT_P
+    save_settings()
     update.message.reply_text(
         'Reply Possibility in current group is: {0:.6f}'.format(group_settings[update.message.chat.id]))
 
 
-def gepi(bot, update):
+def process_message(update, context):
     if update.message.chat.id not in group_settings:
         group_settings[update.message.chat.id] = DEFAULT_P
-    if update.message.reply_to_message:
-        if update.message.reply_to_message.from_user.id == my_uid:
-            # reply to bot
-            if update.message.text:
-                suffix_word = random.choice(suffix_words)
-                keywords = list()
-                input_words = jieba.posseg.cut(update.message.text)
-                for w in input_words:
-                    if w.flag.startswith('v'):
-                        keywords.append(w.word)
-
-                if keywords:
-                    update.message.reply_text(random.choice(keywords) + suffix_word)
-                else:
-                    update.message.reply_text(update.message.text + suffix_word)
-            elif update.message.sticker:
-                sticker_set = bot.get_sticker_set(update.message.sticker.set_name)
-                reply_sticker = random.choice(sticker_set.stickers)
-                update.message.reply_sticker(reply_sticker)
-    elif update.message.forward_from:
-        if update.message.forward_from.id == my_uid:
-            # forward words of bot
-            suffix_word = random.choice(suffix_words)
-            update.message.reply_text('Forward' + suffix_word)
-
-    elif random.random() <= group_settings[update.message.chat.id]:
-        # normal mode
+    if random.random() <= group_settings[update.message.chat.id]:
         if random.random() < 0.5:
-            suffix_word = random.choice(suffix_words)
-            keywords = list()
-            input_words = jieba.posseg.cut(update.message.text)
-            for w in input_words:
-                if w.flag.startswith('v') or w.flag.startswith('a') or w.flag == 'i':
-                    keywords.append(w.word)
-
-            if keywords:
-                update.message.reply_text(random.choice(keywords) + suffix_word)
+            # insult
+            reply_text = generate_insult_text(update.message.text)
+            if reply_text:
+                update.message.reply_text(reply_text)
         else:
+            # repeat
             update.message.reply_text(generate_repeat_text(update.message.text), quote=False)
     print(update.message.chat.id, update.message.chat.title, update.message.from_user.id,
           update.message.from_user.full_name)
+
+
+def reply_mention(update, context):
+    mentions = update.message.parse_entities([MessageEntity.MENTION])
+    for v in mentions.values():
+        if '@' + bot_info.username in v:
+            update.message.reply_sticker(STICKER_FILE_ID)
+            return
+
+
+def reply_reply(update, context):
+    if update.message.reply_to_message.from_user.id != bot_info.id or not update.message.text:
+        return
+    # reply to message replied to bot
+    suffix_word = random.choice(suffix_words)
+    keywords = list()
+    input_words = jieba.posseg.cut(update.message.text)
+    for w in input_words:
+        if w.flag.startswith('v'):
+            keywords.append(w.word)
+
+    if keywords:
+        update.message.reply_text(random.choice(keywords) + suffix_word)
+    else:
+        update.message.reply_text(update.message.text + suffix_word)
+
+
+def reply_forward(update, context):
+    if update.message.forward_from.id != bot_info.id:
+        return
+    # forward words of bot
+    suffix_word = random.choice(suffix_words)
+    update.message.reply_text('Forward' + suffix_word)
+
+
+def reply_sticker(update, context):
+    # reply to stickers replied to bot
+    sticker_set = context.bot.get_sticker_set(update.message.sticker.set_name)
+    sticker = random.choice(sticker_set.stickers)
+    update.message.reply_sticker(sticker)
 
 
 def load_settings():
@@ -118,6 +143,7 @@ def load_settings():
         if os.path.isfile('settings.json'):
             with open('settings.json') as f:
                 group_settings = json.load(f)
+                group_settings = {int(k): v for k, v in group_settings.items()}
         else:
             print('ERROR: settings.json does not exist')
 
@@ -131,13 +157,17 @@ def save_settings():
 def main():
     bot_token = os.environ.get('BOT_TOKEN')
     load_settings()
-    updater = Updater(bot_token)
+    updater = Updater(bot_token, use_context=True)
     updater.dispatcher.add_handler(CommandHandler('test', test))
     updater.dispatcher.add_handler(CommandHandler('setp', set, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('getp', get))
-    updater.dispatcher.add_handler(MessageHandler((Filters.text | Filters.sticker) & Filters.group, gepi))
-    global my_uid
-    my_uid = updater.bot.get_me().id
+    updater.dispatcher.add_handler(MessageHandler(Filters.sticker & Filters.group, reply_sticker))
+    updater.dispatcher.add_handler(MessageHandler(Filters.reply & Filters.group, reply_reply))
+    updater.dispatcher.add_handler(MessageHandler(Filters.forwarded & Filters.group, reply_forward))
+    updater.dispatcher.add_handler(MessageHandler(Filters.entity(MessageEntity.MENTION) & Filters.group, reply_mention))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text & Filters.group, process_message))
+    global bot_info
+    bot_info = updater.bot.get_me()
     updater.start_polling()
     updater.idle()
 
